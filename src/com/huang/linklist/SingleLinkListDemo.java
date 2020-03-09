aa0cc7da273651349c31288c30cd467a64044beb
xuanjinya <1067998193@qq.com>
2020-03-09 13:57:10 +0800

    简单链表的实现,更新删除

--- src/com/huang/linklist/SingleLinkListDemo.java
+++ src/com/huang/linklist/SingleLinkListDemo.java
@@ -16,6 +16,19 @@ public class SingleLinkListDemo {
         singleLinkList.add(heroNode2);
         //显示
         singleLinkList.list();
+        System.out.println("-----------------------------------------------------");
+        //测试修改节点
+        //创建一个新的节点
+        HeroNode newHeroNode = new HeroNode(2, "小卢", "玉麒麟1234");
+        singleLinkList.update(newHeroNode);
+        singleLinkList.list();
+        System.out.println("-----------------------------------------------------");
+        singleLinkList.delete(4);
+        singleLinkList.delete(2);
+        singleLinkList.delete(1);
+        singleLinkList.delete(3);
+        singleLinkList.delete(3);
+        singleLinkList.list();
     }
 }
 
@@ -52,6 +65,62 @@ class SingleLinkList {
             //添加到temp的后面
             heroNode.next = temp.next;
             temp.next = heroNode;
+        }
+    }
+
+    //修改节点的信息，不能修改编号，No不能更改
+    public void update(HeroNode newHeroNode) {
+        //判断链表是否为空
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        //找到需要修改的节点
+        //1.定义一个辅助变量
+        HeroNode temp = head.next;
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                //到达链表最后，遍历结束
+                break;
+            }
+            if (temp.no == newHeroNode.no) {
+                //找到了
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.name = newHeroNode.name;
+            temp.nickname = newHeroNode.nickname;
+        } else {
+            System.out.printf("没有找到编号为%d节点! \n", newHeroNode.no);
+        }
+    }
+
+    //删除节点 先找到节点 temp.next = temp.next.next 剩下的垃圾节点会被回收
+    //因此我们需要找到一个temp辅助节点，找到需要删除的节点的前一个节点
+    public void delete(int no) {
+        HeroNode temp = head;
+        boolean flag = false;//是否找到
+        while (true) {
+            if (temp.next == null) {
+                //已经到链表的最后
+                break;
+            }
+
+            if (temp.next.no == no) {
+                //找到了待删除的节点的前一个节点 temp
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.printf("要删除的%d这个节点不存在！", no);
         }
     }
 
